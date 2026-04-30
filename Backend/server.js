40d90e7084933d7e4b296cea322ae7b5c5c501d9
Avinash <avinash6396singh@gmail.com>
2026-04-30 09:39:09 +0530

    comment dns setting

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
-const dns = require('node:dns/promises');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+// const dns = require('node:dns/promises');
+// dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 require("dotenv").config()
 const app = require("./src/app")
