c4f1d5fb1f5bf765f17a91867f1fb59a9e34e8e7
Avinash <avinash6396singh@gmail.com>
2026-04-30 10:29:42 +0530

    test endpoint

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -26,6 +26,10 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the CareerPilot AI")
+})
+
 
 /* using all the routes here */
 app.use("/api/auth", authRouter)
